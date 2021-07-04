a145ba6136894ad4f4241047f4b43297da179866
tomoki_swanson <tomoki.developer14@gmail.com>
2021-07-04 15:40:04 +0900

    Initial Commit

---
+++ aya_dentaku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  aya_dentaku
+//
+//  Created by snail_cat on 2021/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
